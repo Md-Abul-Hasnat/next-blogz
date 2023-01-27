5fa9183513626d4c9f871cdaeb86d8642d85aef7
Hasnat <webdevhasnat@gmail.com>
2023-01-27 08:30:02 +0600

    all blog done

--- pages/allBlogs/index.jsx
+++ pages/allBlogs/index.jsx
@@ -1,7 +1,6 @@
 import {
   collection,
   query,
-  orderBy,
   startAfter,
   limit,
   getDocs,
@@ -10,18 +9,23 @@ import { db } from "../../components/firebaseConfig";
 import BlogCard from "../../components/BlogCard";
 import style from "../../styles/AllBlogs.module.css";
 import { useEffect, useState } from "react";
+import { toast } from "react-toastify";
 
 const AllBlogs = () => {
   const [allBlogs, setAllBlogs] = useState([]);
+  const [blogsLength, setBlogLength] = useState(0);
   const [lastDoc, setLastDoc] = useState();
+  const [error, setError] = useState(false);
+  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
     async function getData() {
+      setLoading(true);
       const first = query(collection(db, "blogs"), limit(9));
       const documentSnapshots = await getDocs(first);
       const data = documentSnapshots.docs.map((doc) => doc.data());
       setAllBlogs(data);
-
+      setLoading(false);
       const lastDocument =
         documentSnapshots.docs[documentSnapshots.docs.length - 1];
       setLastDoc(lastDocument);
@@ -39,17 +43,33 @@ const AllBlogs = () => {
       documentSnapshots.docs[documentSnapshots.docs.length - 1];
 
     setLastDoc(lastDocument);
+    setBlogLength([...allBlogs, ...data].length);
+    console.log(allBlogs, blogsLength);
+    if (allBlogs.length === blogsLength) {
+      setError(true);
+      toast.success("No more blogs available!!");
+    }
   }
 
   return (
-    <section className={style.allBlogs}>
-      <main className={style.allBlogsWrapper}>
-        {allBlogs?.map((blog, i) => {
-          return <BlogCard data={blog.blogData} key={i} />;
-        })}
-      </main>
-      <button onClick={nextPageData}>Next</button>
-    </section>
+    <>
+      {loading ? (
+        <img className={style.loading} src="/loading.gif" alt="loading" />
+      ) : (
+        <section className={style.allBlogs}>
+          <main className={style.allBlogsWrapper}>
+            {allBlogs?.map((blog, i) => {
+              return <BlogCard data={blog.blogData} key={i} />;
+            })}
+          </main>
+          {!error && (
+            <button className={style.loadBtn} onClick={nextPageData}>
+              Load more
+            </button>
+          )}
+        </section>
+      )}
+    </>
   );
 };
 
