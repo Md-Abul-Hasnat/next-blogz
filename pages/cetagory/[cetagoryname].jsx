bcceaf2383a76b6a46adc10083b6ccfb75e4af57
Hasnat <webdevhasnat@gmail.com>
2023-01-31 18:59:26 +0600

    pagination done

--- pages/cetagory/[cetagoryname].jsx
+++ pages/cetagory/[cetagoryname].jsx
@@ -3,21 +3,35 @@ import { collection, getDocs } from "firebase/firestore";
 import { useRouter } from "next/router";
 import BlogCard from '../../components/BlogCard'
 import style from '../../styles/Cetagory.module.css'
+import { useState } from "react";
 
 
 const Cetagory = ({allBlogs}) => {
+
   const router = useRouter()
-  const selectedCetagory = allBlogs?.filter(blog => blog.blogData.cetagory === router.query.cetagoryname )
-    
-  
+  const selectedCetagoryBlogs = allBlogs?.filter(blog => blog.blogData.cetagory === router.query.cetagoryname )
+  const [showBlog,setShowBlog] = useState(selectedCetagoryBlogs.slice(0,12))
+
+
+  function loadMore(){
+    if(showBlog.length === 12) setShowBlog(selectedCetagoryBlogs.slice(0,24))
+    if(showBlog.length === 24) setShowBlog(selectedCetagoryBlogs.slice(0,36))
+    if(showBlog.length === 36) setShowBlog(selectedCetagoryBlogs.slice(0,48))
+    if(showBlog.length === 48) setShowBlog(selectedCetagoryBlogs.slice(0,60))
+  }
+
   return (
    <section className={style.cetagory}>
     <h1 className={style.cetagoryName}> {router.query.cetagoryname} </h1>
     <main className={style.cetagoryWrapper}>
-      {
-        selectedCetagory?.map((blog,i) => <BlogCard data={blog.blogData} key={i} /> )
+      {selectedCetagoryBlogs.length === 0 ? <h1 className={style.notFound}>No blog found !</h1> :
+        showBlog.map((blog,i) => <BlogCard data={blog.blogData} key={i} /> )
       }
     </main>
+    {
+     showBlog.length === selectedCetagoryBlogs.length ? "" : <button className={style.load} onClick={loadMore}>Load more</button> 
+    }
+     
    </section>
   )
 }
