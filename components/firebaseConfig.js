56567c35c98e91720b6b8497b000335afdb7f80c
Hasnat <webdevhasnat@gmail.com>
2023-02-13 09:38:49 +0600

    done

--- components/firebaseConfig.js
+++ components/firebaseConfig.js
@@ -4,12 +4,12 @@ import { getStorage } from "firebase/storage";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: `${process.env.FIREBASE_API_KEY}`,
-  authDomain: `${process.env.FIREBASE_AUTHDOMAIN}`,
+  apiKey: `${process.env.REACT_APP_FIREBASE_API_KEY}`,
+  authDomain: `${process.env.REACT_APP_FIREBASE_AUTHDOMAIN}`,
   projectId: "next-blogz",
-  storageBucket: `${process.env.FIREBASE_STORAGE_BUCKET}`,
-  messagingSenderId: `${process.env.FIREBASE_MESSAGING_SENDER_ID}`,
-  appId: `${process.env.FIREBASE_APP_ID}`,
+  storageBucket: `${process.env.REACT_APP_FIREBASE_STORAGE_BUCKET}`,
+  messagingSenderId: `${process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID}`,
+  appId: `${process.env.REACT_APP_FIREBASE_APP_ID}`,
 };
 
 // Initialize Firebase
